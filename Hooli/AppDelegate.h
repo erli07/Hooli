783dd278f554a21b099776b5a03319fe2eefb716
Er Li <erli.0715@gmail.com>
2014-10-25 21:53:52 -0400

    Initial Commit

---
+++ Hooli/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hooli
+//
+//  Created by Er Li on 10/25/14.
+//  Copyright (c) 2014 ErLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hooli/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hooli
+//
+//  Created by Er Li on 10/25/14.
+//  Copyright (c) 2014 ErLi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
